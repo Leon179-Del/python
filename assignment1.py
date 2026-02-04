545b4a8449b3b289baeab673add77f453461c139
unknown <leonlangat179@gmail.com>
2026-02-04 14:31:20 +0300

    added update

---
+++ assignment1.py
@@ -0,0 +1,19 @@
+weight = 78
+height = 1.75
+BMI = weight / height**2
+print("the BMI of the person is:",BMI)
+
+# Area of rectangle
+
+length = 48
+width = 25
+Area = length * width
+print("the area of the rectangle is:",Area)
+
+
+# Perimeter
+
+length = 48
+width = 25
+perimeter = length*2 + width*2
+print("the perimeter of the rectangle is:",perimeter)
---
+++ lesson1d.py
@@ -0,0 +1,13 @@
+# create a python program that is able to calculate the simple intrest given the principle as 50000;rate as 5% and time of 8 years. SI = (p*R*t)/100
+principle = 50000
+rate = 5/100
+time =8
+simpleinterest = (principle * rate * time) /100
+print("the simple intrest is:",simpleinterest)
+
+
+
+# Assignment
+# 1. Write a python program that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 meters. BMI = (weight) / (height squared)
+# 2. Find the Area and perimeter of a rectangle whose length is 48cm and width is 25cm
+# Research on python list, tuple and Dictionary Data types.

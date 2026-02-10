ae837247d810a67a479fe70aa46a690c65c418cf
unknown <leonlangat179@gmail.com>
2026-02-10 10:48:06 +0300

    added assignment

---
+++ assignment4.py
@@ -0,0 +1,18 @@
+colors = ["Blue", "Green", "Red", "Pink", "Black"]
+print(colors)
+for color in colors:
+    print(color)
+if "Red" in colors:
+    print("color present")
+else:
+    print("not found")        
+
+
+
+
+    print('====================')
+
+number = 20 
+while number >= 1:
+    print(number)
+    number = number - 1
